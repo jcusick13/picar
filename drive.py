a6aa459666b1b137398790367e7ebf6c4b7244db
Jon Cusick <jonathan.cusick09@gmail.com>
2017-06-17 14:25:58 -0400

    Added IR sensor to Car

--- drive.py
+++ drive.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+import random
 
 
 class Wheel(object):
@@ -31,12 +32,31 @@ class Wheel(object):
         GPIO.output(self.b, 0)
 
 
+class IRSensor(object):
+
+    def __init__(self, signal):
+        """BCM GPIO pin number for digital signal from
+        a connected IR sensor. Signal returns 0 when an object
+        is within a specific range and 1 when no detectable
+        object is in range.
+        """
+        self.signal = signal
+
+        # Attach pull up sensor to avoid missing a signal from noise
+        GPIO.setup(self.signal, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    def read(self):
+        """Reads input signal from sensor, returns value."""
+        return GPIO.input(self.signal)
+
+
 class Car():
 
-    def __init__(self, frontleft, frontright):
-        """Requires two Wheel objects."""
+    def __init__(self, frontleft, frontright, ir_sensor=None):
+        """Requires two Wheels and an IRSensor"""
         self.fl = frontleft
         self.fr = frontright
+        self.ir = ir_sensor
 
     def forward(self, length):
         """Drives forward for the length in seconds."""
@@ -77,11 +97,15 @@ def main():
 
     frontleft = Wheel(14, 15, 18)
     frontright = Wheel(17, 27, 22)
+    ir_sensor = IRSensor(2)
 
-    car = Car(frontleft, frontright)
+    car = Car(frontleft, frontright, ir_sensor)
 
-    car.forward(3)
-    car.reverse(3)
+    while True:
+        if car.ir.read():
+            car.forward(3)
+        else:
+            car.turn_right(random.uniform(0.3, 4))
 
 
 if __name__ == "__main__":
